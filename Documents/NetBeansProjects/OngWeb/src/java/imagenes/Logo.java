8c83c1bc8a98a215d0699351d28573b81414440b
Walter2021-debug <proyectoDAM2.2021@gmail.com>
2021-04-29 19:36:52 +0200

    Desarollando las interfaces de la aplicacion web

---
+++ Documents/NetBeansProjects/OngWeb/src/java/imagenes/Logo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagenes;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Logo extends javax.swing.JPanel {
+    int x, y;
+
+    public Logo(JPanel jPanel1) {
+        this.x = jPanel1.getWidth();
+        this.y = jPanel1.getHeight();
+        this.setSize(x, y);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        ImageIcon Img = new ImageIcon(getClass().getResource("/imagen/logo.jpg"));
+        g.drawImage(Img.getImage(), 0, 0, x, y, null);
+    }
+    
+    
+    
+}
